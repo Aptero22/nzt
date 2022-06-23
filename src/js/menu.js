d0e49c3724e23858a467a4092f656eb220bf8463
Stanislav <staslapa22@gmail.com>
2022-06-24 03:45:58 +0700

    без адаптива

---
+++ src/js/menu.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.header-wrapper');
+const header = document.querySelector('header');
+const options = {
+    rootMargin: '-12%'
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            nav.classList.remove('changeColor')
+        } else {
+            nav.classList.add('changeColor')
+        }
+    })
+}, options)
+
+observer.observe(header)
---
+++ src/js/slider.js
@@ -0,0 +1,77 @@
+"use strict";
+window.addEventListener('DOMContentLoaded', function() {
+
+    const slides = document.querySelectorAll('.slide-wrapper'),
+          prev = document.querySelector('.product-shell__arrows-prev'),
+          next = document.querySelector('.product-shell__arrows-next'),
+          slidesWrapper = document.querySelector('.product-slider'),
+          slidesField = document.querySelector('.product-slider__inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    slidesField.style.width = 100 * slides.length + '%';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    })
+
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+    });
+
+    const lastSlide = document.querySelectorAll('.last-slider__wrapper'),
+          lastPrev = document.querySelector('.product-shell__arrows-prev-last'),
+          lastNext = document.querySelector('.product-shell__arrows-next-last'),
+          lastWrapper = document.querySelector('.last-slider'),
+          lastField = document.querySelector('.last-slider__inner'),
+          lastWidth = window.getComputedStyle(lastWrapper).width;
+
+    let lastOffset = 0;
+
+    lastField.style.width = 65 * lastSlide.length + '%';
+
+    lastSlide.forEach(slide => {
+        slide.style.width = width;
+    })
+
+    lastNext.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (lastSlide.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        lastField.style.transform = `translateX(-${offset}px)`;
+    });
+
+    lastPrev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (lastSlide.length - 1)
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        lastField.style.transform = `translateX(-${offset}px)`;
+    });
+
+
+});
+
